72ab5d25154623d1e356da9855339c0a3ff46313
Taisen.fr (Dev) <vaca@hotmail.fr>
2025-04-06 10:53:00 +0200

    create functions

--- CWToCS8.py
+++ CWToCS8.py
@@ -35,13 +35,22 @@ import sys
 import numpy
 import matplotlib.pyplot as plt
 
-if len(sys.argv) != 3:
-	print("Usage: <script> <message> <output file>")
-	sys.exit(0)
+def make_baseband_samples(amplitude, length_units):
 
-message = sys.argv[1]
+	sample_rate = 8000000
+	unit_seconds = 0.1
+	frequency = 1000.0
 
-character_to_symbols_map = {
+	length_samples = int(round(unit_seconds * length_units * sample_rate))
+	k = 2 * numpy.pi * frequency / sample_rate
+	w = k * numpy.arange(length_samples)
+	return numpy.exp(w * 1j) * amplitude
+
+
+
+def convert_to_CW(message) :
+
+	character_to_symbols_map = {
 	'A': '.-',
 	'B': '-...',
 	'C': '-.-.',
@@ -94,64 +103,73 @@ character_to_symbols_map = {
 	'@': '.--.-.',
 	'<' : '-.-.-', # begin transmission
 	'>' : '.-.-.' # end transmission
-}
+	}
+
+	amplitude = 127
+	dot_units = 1
+	dash_units = dot_units*3
+	space_internal_units = 1
+	space_letters_units = 3
+	space_words_units = 7
+
+	baseband_dot = make_baseband_samples(1, dot_units)
+	baseband_dash = make_baseband_samples(1, dash_units)
+	baseband_between_symbols = make_baseband_samples(0, space_internal_units)
+	baseband_between_letters = make_baseband_samples(0, space_letters_units - space_internal_units)
+	baseband_space = make_baseband_samples(0, space_words_units - space_letters_units - space_internal_units)
+
+	symbol_to_baseband_map = {
+		'.': baseband_dot,
+		'-': baseband_dash,
+		' ': baseband_space,
+	}
+
+	# Start with a little silence.
+	output = [baseband_space]
+
+	for character in '< '+ message.upper() +' >': # add "<" and ">" to respect convention
+		symbols = character_to_symbols_map[character]
+		for symbol in symbols:
+			output.append(symbol_to_baseband_map[symbol])
+			output.append(baseband_between_symbols)
+		output.append(baseband_between_letters)
+
+	# Append a little extra silence at the end.
+	output.append(baseband_space)
+	output = numpy.concatenate(output) * amplitude
+
+	return output
+
+def write_toCS8(IQ, file ):
+
+	output_int = numpy.empty((len(IQ) * 2,), dtype=numpy.int8)
+	output_int[0::2] = numpy.round(IQ.real)
+	output_int[1::2] = numpy.round(IQ.imag)
+	output_int.tofile(file)
 
 
-def make_baseband_samples(amplitude, length_units):
-	frequency = 1000.0
-	length_samples = int(round(unit_seconds * length_units * sample_rate))
-	k = 2 * numpy.pi * frequency / sample_rate
-	w = k * numpy.arange(length_samples)
-	return numpy.exp(w * 1j) * amplitude
+if __name__ == "__main__":
 
-sample_rate = 8000000
-unit_seconds = 0.1
-amplitude = 127
+	if len(sys.argv) != 3:
+		print("Usage: <script> <message> <output file>")
+		sys.exit(0)
 
-dot_units = 1
-dash_units = dot_units*3
-space_internal_units = 1
-space_letters_units = 3
-space_words_units = 7
+	output = convert_to_CW(sys.argv[1])
 
-baseband_dot = make_baseband_samples(1, dot_units)
-baseband_dash = make_baseband_samples(1, dash_units)
-baseband_between_symbols = make_baseband_samples(0, space_internal_units)
-baseband_between_letters = make_baseband_samples(0, space_letters_units - space_internal_units)
-baseband_space = make_baseband_samples(0, space_words_units - space_letters_units - space_internal_units)
+	# verify
+	try:
+		plt.plot(output)
+		plt.legend(loc="upper right")
+		plt.show()
+	except Exception:
+		print(Exception)
 
-symbol_to_baseband_map = {
-	'.': baseband_dot,
-	'-': baseband_dash,
-	' ': baseband_space,
-}
+	write_toCS8(output, sys.argv[2])
 
-# Start with a little silence.
-output = [baseband_space]
 
-for character in '< '+ message.upper() +' >': # add "<" and ">" to respect convention
-	symbols = character_to_symbols_map[character]
-	for symbol in symbols:
-		output.append(symbol_to_baseband_map[symbol])
-		output.append(baseband_between_symbols)
-	output.append(baseband_between_letters)
 
-# Append a little extra silence at the end.
-output.append(baseband_space)
-output = numpy.concatenate(output) * amplitude
 
-# verify
-try:
-	plt.plot(output)
-	plt.legend(loc="upper right")
-	plt.show()
-except Exception:
-    print(Exception)
 
-# write
-output_int = numpy.empty((len(output) * 2,), dtype=numpy.int8)
-output_int[0::2] = numpy.round(output.real)
-output_int[1::2] = numpy.round(output.imag)
 
-output_int.tofile(sys.argv[2])
+
 
