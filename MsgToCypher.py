bc4c19399337bb2b139aee061159adcb375835e6
Taisen.fr (Dev) <vaca@hotmail.fr>
2025-04-07 09:39:33 +0200

    POC-v1

--- MsgToCypher.py
+++ MsgToCypher.py
@@ -5,7 +5,7 @@
 # Encrypt :
 #  
 # python ./MsgToCypher.py test
-# python ./MsgToCypher.py enc test 3S8C/nPgfAEu0HAxsZQmz3bWfowPIjtbHAv038b6Nvk= K3kFByTQGpDPnyUIdKLFrg==
+# python ./MsgToCypher.py enc test 9CEA372979FFDCBA028BD523A3F43A44B527DE31E2BBAE56F641D87D3F6C80BC A977EA111934D65E8A6B5AC3D52B82F8
 #
 # Decrypt : 
 #
@@ -14,8 +14,6 @@
 # /!\ Be aware that IVs should not be used twice ! 
 #
 
-
-import base64
 import secrets
 import sys
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -25,7 +23,6 @@ from cryptography.hazmat.backends import default_backend
 
 # Chiffrer une chaîne de caractères
 def chiffre_message(cle, iv, message):
-
     padder = padding.PKCS7(128).padder()
     message = message.encode()
     message = padder.update(message) + padder.finalize()
@@ -36,7 +33,6 @@ def chiffre_message(cle, iv, message):
 
 # Déchiffrer un message
 def dechiffre_message(cle, iv, ct):
-    
     cipher = Cipher(algorithms.AES(cle), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     message = decryptor.update(ct) + decryptor.finalize()
@@ -45,13 +41,11 @@ def dechiffre_message(cle, iv, ct):
     return message.decode()
 
 
-
 if __name__ == "__main__":
-
     if len(sys.argv) != 2 and len(sys.argv) != 5:
         print("Usage to encrypt with random key: <script> <msg>")
-        print("Usage to encrypt: <script> enc <msg> <key_base64> <iv_base64>")
-        print("Usage to decrypt: <script> dec <cipher_base64> <key_base64> <iv_base64>")
+        print("Usage to encrypt: <script> enc <msg> <key_hex> <iv_hex>")
+        print("Usage to decrypt: <script> dec <cipher_hex> <key_hex> <iv_hex>")
         sys.exit(0)
 
     if len(sys.argv) == 2:
@@ -59,24 +53,23 @@ if __name__ == "__main__":
         k = secrets.token_bytes(32)  # génération d'une clé AES
         i = secrets.token_bytes(16)  # génération d'IVs
         c = chiffre_message(k, i, m)
-        print("key:", base64.b64encode(k).decode())
-        print("iv:", base64.b64encode(i).decode())
-        print("cipher:", base64.b64encode(c).decode())
+        print("key:", k.hex().upper())
+        print("iv:", i.hex().upper())
+        print("cipher:", c.hex().upper())
 
     if len(sys.argv) == 5:
-
         if sys.argv[1] == 'enc':
             m = sys.argv[2]
-            k = base64.b64decode(sys.argv[3])
-            i = base64.b64decode(sys.argv[4])
+            k = bytes.fromhex(sys.argv[3])
+            i = bytes.fromhex(sys.argv[4])
             c = chiffre_message(k, i, m)
-            print("key:", base64.b64encode(k).decode())
-            print("iv:", base64.b64encode(i).decode())
-            print("cipher:", base64.b64encode(c).decode())
+            print("key:", k.hex().upper())
+            print("iv:", i.hex().upper())
+            print("cipher:", c.hex().upper())
 
         if sys.argv[1] == 'dec':
-            c = base64.b64decode(sys.argv[2])
-            k = base64.b64decode(sys.argv[3])
-            i = base64.b64decode(sys.argv[4])
+            c = bytes.fromhex(sys.argv[2])
+            k = bytes.fromhex(sys.argv[3])
+            i = bytes.fromhex(sys.argv[4])
             m = dechiffre_message(k, i, c)
             print("message:", m)
--- README.md
+++ README.md
@@ -1,5 +1,87 @@
+[![License: GNU GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Github Sponsors](https://img.shields.io/badge/GitHub%20Sponsors-30363D?&logo=GitHub-Sponsors&logoColor=EA4AAA)](https://github.com/sponsors/Aif4thah/)
+
 # SecCW
 
 Secured Continuous Wave transmission
 
-> This is a non-standardized POC at this time. Use it at your own risk.
+
+> 🤝 **Ethical Sponsorship Request** : While this project is open-source and free to use, we kindly ask that if you are using it for commercial purposes or in a business setting, please consider sponsoring the project through GitHub Sponsors. Your support helps maintain and improve the project, ensuring it remains a valuable resource for everyone. Thank you for your understanding and generosity!
+
+> ⚠️ **Disclaimer** : **This is a non-standardized POC at this time. Use it at your own risk.**, I do not guarantee any form of confidentiality or authenticity without further study. This repository and its tools are provided "as is." The author(s) make no representations or warranties, express or implied, regarding the operation of the information, content, materials, tools, services, or products included. The author(s) disclaim, to the full extent permissible by law, all warranties, express or implied, including implied warranties of merchantability and fitness for a particular purpose.
+
+
+## Quick Demo
+
+To leverage this proof of concept, I suggest using an LNA as an RF amplifier to gain a small amount of power (less than 0.5W). **Ensure compliance with local regulations**.
+
+<p align="center">
+    <img src="./HackRF-LaNa-Transmit.jpg" alt="HackRF-Lan" style="width: 800px;" />
+</p>
+
+## Transmit
+
+```sh
+python ./MsgToCypher.py enc test 9CEA372979FFDCBA028BD523A3F43A44B527DE31E2BBAE56F641D87D3F6C80BC A977EA111934D65E8A6B5AC3D52B82F8
+key: 9CEA372979FFDCBA028BD523A3F43A44B527DE31E2BBAE56F641D87D3F6C80BC
+iv: A977EA111934D65E8A6B5AC3D52B82F8
+
+# cipher: EFAADCF7EA0A786EF7B4EF7504605970
+```
+
+
+```sh
+python ./CWToCS8.py EFAADCF7EA0A786EF7B4EF7504605970 test.cs8
+```
+
+<p align="center">
+    <img src="./Plot.png" alt="PltBlue" style="width: 600px;" />
+</p>
+
+```sh
+hackrf_transfer -s 8000000 -x 16 -a 1 -f 40677000 -b 1750000 -t test.cs8
+```
+
+## Recieve
+
+* Receive with HackRF or another material like a Talkie-Walkie and Decode Morse
+
+```pyhton
+python ./ReadCS8.py ./test.cs8
+```
+
+<p align="center">
+    <img src="./PlotRed.png" alt="PltRed" style="width: 600px;" />
+</p>
+
+
+* Decrypt
+
+```sh
+python ./MsgToCypher.py dec EFAADCF7EA0A786EF7B4EF7504605970 9CEA372979FFDCBA028BD523A3F43A44B527DE31E2BBAE56F641D87D3F6C80BC A977EA111934D65E8A6B5AC3D52B82F8
+
+# message: test
+```
+
+
+## Misc
+
+### Python
+
+* version: 3.13.2.
+* Virtual Env: read `requirement.txt`
+
+### HackRF-One
+
+* Binaries: 2024.02.1
+* Firmware Version: 2024.02.1
+
+## Go further...
+
+* Read Scripts
+
+* More info about DSP and RF on [Dojo-101](https://github.com/Aif4thah/Dojo-101)
+
+## Credits
+
+* Special thanks to Jared Boone (ShareBrained Technology) for his original script, without him the development would have been much longer !
